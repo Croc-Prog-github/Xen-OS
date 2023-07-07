5b1ea449c1ee3801bf313aa8fabc17321b57f185
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-07-07 23:32:42 +0200

    4 file added, 1 file rinominated

---
+++ Sistem/ModProv/menu.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"1. Esci e continua su Xen-OS";
+    cout<<"2. Verifica Hardware";
+    cout<<"3. BIOS";
+    cout<<"4. Defrag Hardisk";
+    cout<<"5. Ripristina Xen-OS";
+    cout<<"6. Ripristina Hardisk";
+}
