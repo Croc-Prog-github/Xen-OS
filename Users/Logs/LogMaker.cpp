f05ac7be113bbd759506923796ec08226a370d1d
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-07-07 22:24:04 +0200

    2 file edited

---
+++ Users/Logs/LogMaker.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void MakeFile(const string& arg) {
+    string filePath;
+
+    if (arg == "1") {
+        filePath = "/Users/Logs/SystemLogs/SystemLog.log";
+    } else if (arg == "2") {
+        filePath = "/Users/Logs/HardwareLogs/HardwareLog.log";
+    } else {
+        cout << "Errore nella funzione. Creazione del file non riuscita." <<endl;
+        return;
+    }
+
+    ofstream file(filePath);
+
+    if (file.is_open()) {
+        file << "This is a sample file." <<endl;
+        file.close();
+        cout << "File creato in: " <<filePath <<endl;
+    } else {
+        cout << "Fallimento creazione file" <<endl;
+    }
+}
