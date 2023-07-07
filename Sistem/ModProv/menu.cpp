4fad8b8bf78268585922b9b004a076068b320813
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-07-08 00:00:12 +0200

    2 file edited, 1 file added

--- Sistem/ModProv/menu.cpp
+++ Sistem/ModProv/menu.cpp
@@ -2,10 +2,40 @@
 using namespace std;
 
 int main() {
-    cout<<"1. Esci e continua su Xen-OS";
-    cout<<"2. Verifica Hardware";
-    cout<<"3. BIOS";
-    cout<<"4. Defrag Hardisk";
-    cout<<"5. Ripristina Xen-OS";
-    cout<<"6. Ripristina Hardisk";
+    int n = 1;
+
+    cout<<"1. Esci e continua su Xen-OS" <<endl;
+    cout<<"2. Verifica Hardware" <<endl;
+    cout<<"3. BIOS" <<endl;
+    cout<<"4. Defrag Hardisk" <<endl;
+    cout<<"5. Ripristina Xen-OS" <<endl;
+    cout<<"6. Ripristina Hardisk" <<endl;
+
+    cin>>n;
+    switch(n) {
+        case 1:
+
+        break;
+        case 2:
+
+        break;
+
+        case 3:
+
+        break;
+        case 4:
+
+        break;
+
+        case 5:
+
+        break;
+        case 6:
+
+        break;
+        default:
+            cout<<"Numero non valido, riprova." <<endl;
+            main();
+        break;
+    }
 }
