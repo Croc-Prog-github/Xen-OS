d3a8cb15a9404cce543b0bb3a318f7918c449275
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-07-12 14:20:29 +0200

    1 file edited

--- System/ModProv/menu.cpp
+++ System/ModProv/menu.cpp
@@ -9,36 +9,27 @@ int main() {
     cout<<"3. BIOS" <<endl;
     cout<<"4. Defrag Hardisk" <<endl;
     cout<<"5. Ripristina" <<endl;
-    
-    cout<<"9. "
 
     cin>>n;
     switch(n) {
         case 1:
-
+            //
         break;
         case 2:
-            cout<<"1. Verifica CPU" <<endl;
-            cout<<"2. Verifica GPU" <<endl;
-            cout<<"3. Verifica MB" <<endl;
-            
-
+            cout<<"1. Verifica CPU (Processore)" <<endl;
+            cout<<"2. Verifica GPU (Scheda Video)" <<endl;
+            cout<<"3. Verifica Scheda Madre" <<endl;
         break;
-
         case 3:
-
+            //
         break;
         case 4:
-
+            //
         break;
-
         case 5:
             cout<<"1. Ripristina Xen-OS" <<endl;
             cout<<"2. Ripristina Hardisk" <<endl;
             cout<<"3. Ripristina Scheda Video" <<endl;
-        break;
-        case 6:
-
         break;
         default:
             cout<<"Numero non valido, riprova." <<endl;
