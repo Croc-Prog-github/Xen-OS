f1633ed99732e1315a1f3b00823c7dec336a47c8
ZEUSSTARSOP <122162647+ZEUSSTARSOP@users.noreply.github.com>
2023-07-12 14:16:22 +0200

    1 file edited

--- System/ModProv/menu.cpp
+++ System/ModProv/menu.cpp
@@ -8,8 +8,9 @@ int main() {
     cout<<"2. Verifica Hardware" <<endl;
     cout<<"3. BIOS" <<endl;
     cout<<"4. Defrag Hardisk" <<endl;
-    cout<<"5. Ripristina Xen-OS" <<endl;
-    cout<<"6. Ripristina Hardisk" <<endl;
+    cout<<"5. Ripristina" <<endl;
+    
+    cout<<"9. "
 
     cin>>n;
     switch(n) {
@@ -17,6 +18,10 @@ int main() {
 
         break;
         case 2:
+            cout<<"1. Verifica CPU" <<endl;
+            cout<<"2. Verifica GPU" <<endl;
+            cout<<"3. Verifica MB" <<endl;
+            
 
         break;
 
@@ -28,7 +33,9 @@ int main() {
         break;
 
         case 5:
-
+            cout<<"1. Ripristina Xen-OS" <<endl;
+            cout<<"2. Ripristina Hardisk" <<endl;
+            cout<<"3. Ripristina Scheda Video" <<endl;
         break;
         case 6:
 
